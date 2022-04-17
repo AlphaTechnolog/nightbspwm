110b5ba99cc19507a52b8db7d27038ed07c46b14
Gabriel Guerra <gfranklings@gmail.com>
2022-04-17 18:15:20 -0400

    beautified more configurations && and more automations && added more simply stuff and fixes

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -2,5 +2,20 @@
 
 pkill polybar
 
-polybar main -r -c $HOME/.config/polybar/config.ini &
-polybar secondary -r -c $HOME/.config/polybar/config.ini &
+get_monitors () {
+  xrandr | \
+    grep -w 'connected' | \
+    awk '{print $1}'
+}
+
+multimonitor_support () {
+  for monitor in $(get_monitors); do
+    MONITOR=$monitor polybar main -r -c $HOME/.config/polybar/config.ini &
+  done
+}
+
+if type "xrandr"; then
+  multimonitor_support
+else
+  polybar main -r -c $HOME/.config/polybar/config.ini &
+fi
